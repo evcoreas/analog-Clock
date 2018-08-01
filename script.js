4964bb4190992215cd8aef6128e5ce8a83202a5e
evcoreas <evcoreas@live.com>
2018-07-31 23:35:00 -0400

    fixed hour hand transition

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-let hrPositon = (hr * 360/12) + (min * (360/60) / 12);
-let minPosition = (min * 360/60) + (sec * (360/60) / 60);
+let hrPosition = (hr * 360/12) + (min * (360/60)/12);
+let minPosition = (min*360/60) + (sec * (360/60)/60);
 let secPosition = sec * 360/60;
 
 function runAnalogClock() {
-  hrPositon += (3/360);
-  minPosition += (6/60);
-  secPosition += 6;
+    hrPosition += (3/360);
+    minPosition += (6/60);
+    secPosition += 6;
 
-  HRHAND.style.transform = "rotate(" + hrPositon + " deg)";
-  MINHAND.style.transform = "rotate(" + minPosition + "deg)";
-  SECHAND.style.transform = "rotate(" + secPosition + "deg)";
+    HRHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
 var interval = setInterval(runAnalogClock, 1000);
