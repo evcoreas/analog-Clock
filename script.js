7c643b6ee81134d4b8c711fe4451395cd5956024
evcoreas <evcoreas@live.com>
2018-07-31 22:16:49 -0400

    fixed transition and appearance

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const HRHAND = document.querySelector("#hour");
 const MINHAND = document.querySelector("#minute");
 const SECHAND = document.querySelector("#second");
 
-// place the script in a function in order for the clock to run at a given interval
-// instead of reloading the page
-function runAnalogClock() {
-  var date = new Date();
-  console.log(date);
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-  console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-  let hrPositon = (hr * 360/12) + (min * (360/60) / 12);
-  let minPosition = (min * 360/60) + (sec * (360/60) / 60);
-  let secPosition = sec * 360/60;
+let hrPositon = (hr * 360/12) + (min * (360/60) / 12);
+let minPosition = (min * 360/60) + (sec * (360/60) / 60);
+let secPosition = sec * 360/60;
+
+function runAnalogClock() {
+  hrPositon += (3/360);
+  minPosition += (6/60);
+  secPosition += 6;
 
   HRHAND.style.transform = "rotate(" + hrPositon + " deg)";
   MINHAND.style.transform = "rotate(" + minPosition + "deg)";
